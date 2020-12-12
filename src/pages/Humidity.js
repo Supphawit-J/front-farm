544c1de321b5131392eca9e40e731bd32b5a15d7
PHENIXCRIME <photboatxeemus@icloud.com>
2020-12-12 11:08:24 +0700

    CREATE , EDIT humidity

---
+++ src/pages/Humidity.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import LayoutData from '../components/LayoutData'
+
+function Humidity () {
+  return (
+    <>
+        <LayoutData/>
+    </>
+  )
+}
+export default Humidity
