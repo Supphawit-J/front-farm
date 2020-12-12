a99370bfeecfb46c337731e8f234f83d01b78d9f
PHENIXCRIME <photboatxeemus@icloud.com>
2020-12-12 20:45:43 +0700

    EDIT width align-item margin

--- src/components/TitleDashboard.js
+++ src/components/TitleDashboard.js
@@ -3,13 +3,13 @@ import styled from 'styled-components'
 import { ToggleOff } from '@styled-icons/material-twotone/ToggleOff'
 
 const ShowCase = styled.div`
-width : 77vw ;
+width : 78vw ;
 height : 10vh ;
 display : flex ;
 justify-content : space-between ;
 align-items : center ;
 border-radius : 30px ;
-margin : 55px 0 0 0 ; 
+margin : 55px 0 0 0 ;
 `
 
 const TitleName = styled.div`
@@ -22,8 +22,10 @@ cursor: pointer;
 const IconToggle = styled.div`
 color : #ffffff;
 display : flex ;
-align-items : center ; 
+align-items : center ;
+margin : 0 20px 0 0 ;
 cursor: pointer;
+
 `
 const SettingMode = styled.div`
 font-size : 20px;
