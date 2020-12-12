b76f800cb41873e6ca1834dc2e61ebec846c678d
PHENIXCRIME <photboatxeemus@icloud.com>
2020-12-12 11:04:26 +0700

    IMPORT helmet globalstyle and RETURN object

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Reset } from 'styled-reset'
+import withHelmet from './utils/withHelmet'
+import GlobalStyle from './components/GlobalStyle'
+import routes from './config/routes'
+
 function App () {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <GlobalStyle/>
+      <Reset/>
+      <Router>
+        <Suspense fallback = '...loading'>
+        <Switch>
+            {Object.keys(routes).map(routeKey => (
+        <Route key = {routeKey} {...routes[routeKey] }/>))}
+          </Switch>
+        </Suspense>
+      </Router>
+    </>
   )
 }
 
-export default App
+export default withHelmet('ArgoGreen | CAMT DII')(App)
