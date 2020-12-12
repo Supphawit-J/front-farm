97956e4b5bd3fcfe7731dd35f9d038545d870b24
PHENIXCRIME <photboatxeemus@icloud.com>
2020-12-12 19:01:09 +0700

    EDIT position item , ADD item

--- src/components/GraphDisplay.js
+++ src/components/GraphDisplay.js
@@ -15,23 +15,147 @@ height : 55vh ;
 background-color : #404040 ;
 border-radius : 50px ;
 box-shadow: 10px 11px 5px -8px rgba(0,0,0,0.32);
+display : flex ;
+align-items : center ;
+flex-direction : column ;
+justify-content : space-around ;
+`
 
+const AreaGraph = styled.div`
+width : 75vw;
+height : 40vh ;
+background-color : white ;
+box-shadow: 10px 11px 5px -8px rgba(0,0,0,0.32);
+border-radius : 40px ;
+display : flex ;
+align-items : center ;
+`
+
+const SlotBy = styled.div`
+display : flex ;
+justify-content : flex-end ;
+width : 78vw ;
+height : 5vh ;
+`
+
+const GraphToday = styled.div`
+font-size : 25px ;
+color : white ;
+padding : 20px 40px 0 0 ;
+cursor: pointer;
+`
+
+const GraphWeek = styled.div`
+font-size : 25px ;
+color : white ;
+padding : 20px 40px 0 0 ;
+cursor: pointer;
+`
+
+const GraphMonth = styled.div`
+font-size : 25px ;
+color : white ;
+padding : 20px 40px 0 0 ;
+cursor: pointer;
 `
 
 const DipslayBottom = styled.div`
 width : 78vw;
 height : 20vh ;
+display : flex ;
+justify-content : space-between ;
+`
+
+const DisplayMin = styled.div`
+width : 24vw;
+height : 20vh;
 background-color : #404040 ;
 border-radius : 50px ;
 box-shadow: 10px 11px 5px -8px rgba(0,0,0,0.32);
+display : flex ; 
+flex-direction : column ;
+justify-content : space-evenly ;
+`
+
+const DisplayMax = styled.div`
+width : 24vw;
+height : 20vh;
+background-color : #404040 ;
+border-radius : 50px ;
+box-shadow: 10px 11px 5px -8px rgba(0,0,0,0.32);
+display : flex ; 
+flex-direction : column ;
+justify-content : space-evenly ;
+`
+
+const DisplayAvg = styled.div`
+width : 24vw;
+height : 20vh;
+background-color : #404040 ;
+border-radius : 50px ;
+box-shadow: 10px 11px 5px -8px rgba(0,0,0,0.32);
+display : flex ; 
+flex-direction : column ;
+justify-content : space-evenly ;
+`
+const TitleCheck = styled.div`
+color : white ;
+font-size : 30px ;
+margin : 0 0 0 40px ;
+`
+const NumberUnit = styled.div`
+display : flex ;
+flex-direction : row ;
+margin : 0 40px 0 40px ;
+align-items : center ;
+justify-content : space-between ;
+`
+
+const TitleDisplay = styled.div`
+color : white ;
+font-size : 100px ;
+
+`
+const TitleUnit = styled.div`
+color : white ;
+font-size : 30px ;
 `
 
 function GraphDisplay () {
   return (
     <>
     <ShowCase>
-    <DisplayTop></DisplayTop>
-    <DipslayBottom></DipslayBottom>
+    <DisplayTop>
+      <SlotBy>
+        <GraphToday>Today</GraphToday>
+        <GraphWeek>Week</GraphWeek>
+        <GraphMonth>Month</GraphMonth>
+      </SlotBy>
+      <AreaGraph></AreaGraph>
+    </DisplayTop>
+    <DipslayBottom>
+      <DisplayMin>
+        <TitleCheck>Min</TitleCheck>
+        <NumberUnit>
+        <TitleDisplay>10</TitleDisplay>
+        <TitleUnit>Celsius</TitleUnit>
+        </NumberUnit>
+      </DisplayMin>
+      <DisplayMax>
+      <TitleCheck>Max</TitleCheck>
+      <NumberUnit>
+      <TitleDisplay>20</TitleDisplay>
+      <TitleUnit>Celsius</TitleUnit>
+      </NumberUnit>
+      </DisplayMax>
+      <DisplayAvg>
+        <TitleCheck>Average</TitleCheck>
+      <NumberUnit>
+        <TitleDisplay>30</TitleDisplay>
+        <TitleUnit>Celsius</TitleUnit>
+      </NumberUnit>
+      </DisplayAvg>
+    </DipslayBottom>
     </ShowCase>
     </>
   )
