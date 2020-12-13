84af0eb2d92bcceb98d77cce4e191f5588635504
PHENIXCRIME <photboatxeemus@icloud.com>
2020-12-13 13:54:54 +0700

    import Display

--- src/components/GraphDisplay.js
+++ src/components/GraphDisplay.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Display from '../components/DisplayDataGraph/Display'
 
 const ShowCase = styled.div`
 display : flex ;
@@ -66,60 +67,60 @@ display : flex ;
 justify-content : space-between ;
 `
 
-const DisplayMin = styled.div`
-width : 25vw;
-height : 20vh;
-background-color : #404040 ;
-border-radius : 20px ;
-box-shadow: 10px 11px 5px -8px rgba(0,0,0,0.32);
-display : flex ; 
-flex-direction : column ;
-justify-content : space-evenly ;
-`
+// const DisplayMin = styled.div`
+// width : 25vw;
+// height : 20vh;
+// background-color : #404040 ;
+// border-radius : 20px ;
+// box-shadow: 10px 11px 5px -8px rgba(0,0,0,0.32);
+// display : flex ;
+// flex-direction : column ;
+// justify-content : space-evenly ;
+// `
 
-const DisplayMax = styled.div`
-width : 25vw;
-height : 20vh;
-background-color : #404040 ;
-border-radius : 20px ;
-box-shadow: 10px 211px 5px -8px rgba(0,0,0,0.32);
-display : flex ; 
-flex-direction : column ;
-justify-content : space-evenly ;
-`
+// const DisplayMax = styled.div`
+// width : 25vw;
+// height : 20vh;
+// background-color : #404040 ;
+// border-radius : 20px ;
+// box-shadow: 10px 11px 5px -8px rgba(0,0,0,0.32);
+// display : flex ;
+// flex-direction : column ;
+// justify-content : space-evenly ;
+// `
 
-const DisplayAvg = styled.div`
-width : 25vw;
-height : 20vh;
-background-color : #404040 ;
-border-radius : 20px ;
-box-shadow: 10px 11px 5px -8px rgba(0,0,0,0.32);
-display : flex ; 
-flex-direction : column ;
-justify-content : space-evenly ;
-`
-const TitleCheck = styled.div`
-color : white ;
-font-size : 30px ;
-margin : 0 0 0 40px ;
-`
-const NumberUnit = styled.div`
-display : flex ;
-flex-direction : row ;
-margin : 0 40px 0 40px ;
-align-items : center ;
-justify-content : space-between ;
-`
+// const DisplayAvg = styled.div`
+// width : 25vw;
+// height : 20vh;
+// background-color : #404040 ;
+// border-radius : 20px ;
+// box-shadow: 10px 11px 5px -8px rgba(0,0,0,0.32);
+// display : flex ;
+// flex-direction : column ;
+// justify-content : space-evenly ;
+// `
+// const TitleCheck = styled.div`
+// color : white ;
+// font-size : 30px ;
+// margin : 0 0 0 40px ;
+// `
+// const NumberUnit = styled.div`
+// display : flex ;
+// flex-direction : row ;
+// margin : 0 40px 0 40px ;
+// align-items : center ;
+// justify-content : space-between ;
+// `
 
-const TitleDisplay = styled.div`
-color : white ;
-font-size : 100px ;
+// const TitleDisplay = styled.div`
+// color : white ;
+// font-size : 100px ;
 
-`
-const TitleUnit = styled.div`
-color : white ;
-font-size : 30px ;
-`
+// `
+// const TitleUnit = styled.div`
+// color : white ;
+// font-size : 30px ;
+// `
 
 function GraphDisplay () {
   return (
@@ -134,14 +135,28 @@ function GraphDisplay () {
       <AreaGraph></AreaGraph>
     </DisplayTop>
     <DipslayBottom>
-      <DisplayMin>
+      <Display/>
+      {/* <DisplayMin>
         <TitleCheck>Min</TitleCheck>
         <NumberUnit>
         <TitleDisplay>10</TitleDisplay>
         <TitleUnit>Celsius</TitleUnit>
         </NumberUnit>
       </DisplayMin>
-      <DisplayMax>
+      <DisplayMin>
+        <TitleCheck>Min</TitleCheck>
+        <NumberUnit>
+        <TitleDisplay>10</TitleDisplay>
+        <TitleUnit>Celsius</TitleUnit>
+        </NumberUnit>
+      </DisplayMin><DisplayMin>
+        <TitleCheck>Min</TitleCheck>
+        <NumberUnit>
+        <TitleDisplay>10</TitleDisplay>
+        <TitleUnit>Celsius</TitleUnit>
+        </NumberUnit>
+      </DisplayMin> */}
+      {/* <DisplayMax>
       <TitleCheck>Max</TitleCheck>
       <NumberUnit>
       <TitleDisplay>20</TitleDisplay>
@@ -154,7 +169,7 @@ function GraphDisplay () {
         <TitleDisplay>30</TitleDisplay>
         <TitleUnit>Celsius</TitleUnit>
       </NumberUnit>
-      </DisplayAvg>
+      </DisplayAvg> */}
     </DipslayBottom>
     </ShowCase>
     </>
