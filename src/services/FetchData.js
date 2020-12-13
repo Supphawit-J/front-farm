a351b05058628e1411b0b5d908688994167fdac9
panuwat-moonkham <panuwat.moonkham00@gmail.com>
2020-12-14 00:12:19 +0700

    ADD fetch data

--- src/services/FetchData.js
+++ src/services/FetchData.js
@@ -1,61 +1,32 @@
-import { useState } from 'react'
-export function FetchData () {
-  const time = new Date()
-  time.setHours(6)
-  time.setMinutes(0)
-  time.setSeconds(0)
-  const [bigData, setBigdata] = useState([])
+import axios from 'axios'
 
-  async function useData () {
-    for (let i = 0; i < 10; i++) {
-      setBigdata(bigData.push({
-        device_id: 'D5',
-        device_status: 'on',
-        temp: Math.floor((i - (0.9 * i)) + 15 - (0.03 * i)),
-        humidity: Math.floor(Math.random() * 100) + 40,
-        wind: Math.floor(Math.random() * 10),
-        lux: Math.floor(Math.random() * 400) + 50,
-        timestamp: new Date(time.setMinutes(time.getMinutes() + 5))
-      }))
-      console.log(bigData[bigData.length - 1])
+const API_ENDPOINT = 'http://localhost:3333'
+const API_VERSION = 'api/v1'
+// const JSON_HEADER = { 'Content-Type': 'application/json' }
+
+export async function getData (table) {
+  return await axios(
+    {
+      method: 'GET',
+      url: `${API_ENDPOINT}/${API_VERSION}/${table}`
     }
-  }
+  ).then(response => response.data.data)
+}
 
-  // for (let i = 0; i < 144; i++) {
-  //   bigData.push({
-  //     device_id: 'DFG615',
-  //     device_status: 'on',
-  //     temp: Math.floor((25 - (i * 0.05))),
-  //     humidity: Math.floor(Math.random() * 100) + 40,
-  //     wind: Math.floor(Math.random() * 10),
-  //     lux: Math.floor(Math.random() * 400) + 50,
-  //     timestamp: (time.setMinutes(time.getMinutes() + 5))
-  //   })
-  // }
+export async function getDataRef (table, value) {
+  return await axios(
+    {
+      method: 'GET',
+      url: `${API_ENDPOINT}/${API_VERSION}/${table}?references=${value}`
+    }
+  ).then(response => response.data.data)
+}
 
-  async function handleUpdate () {
-    bigData.push({
-      device_id: 'DFG615',
-      device_status: 'on',
-      temp: Math.floor(Math.random() * (30 - 25) + 25),
-      humidity: Math.floor(Math.random() * (30 - 25) + 25),
-      wind: Math.floor(Math.random() * (30 - 25) + 25),
-      lux: Math.floor(Math.random() * (30 - 25) + 25),
-      timestamp: new Date(time.setMinutes(time.getMinutes() + 5))
-    })
-    console.log(bigData[bigData.length - 1], bigData.length)
-  }
-  async function clearData () {
-    setBigdata(bigData.splice(0, bigData.length))
-    console.log(bigData.length)
-  }
-
-  // const CronJob = require('cron').CronJob
-
-  // const job = new CronJob('* * * * * *', function () {
-  //   handleUpdate()
-  //   console.log(bigData[bigData.length - 1].timestamp, bigData.length)
-  // })
-  // job.start()
-  return [{ bigData }, { useData, handleUpdate, clearData }]
+export async function getDataById (table, value, id) {
+  return await axios(
+    {
+      method: 'GET',
+      url: `${API_ENDPOINT}/${API_VERSION}/${table}/${id}?references=${value}`
+    }
+  ).then(response => response.data.data)
 }
