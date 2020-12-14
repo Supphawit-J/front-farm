92fca852f0f6a9fc3ddd23f2199cd0fa90460562
PHENIXCRIME <photboatxeemus@icloud.com>
2020-12-14 21:16:46 +0700

    DELETE space before comma

--- src/pages/Wind.js
+++ src/pages/Wind.js
@@ -7,7 +7,7 @@ function Wind () {
   return (
     <Actioncontext.Provider
       value={{
-        titlename: 'Wind Dashboard' ,
+        titlename: 'Wind Dashboard',
         min: { titlecheck: 'Min', titledisplay: '10', unitcheck: 'Km / hr' },
         max: { titlecheck: 'Max', titledisplay: '20', unitcheck: 'Km / hr' },
         avg: { titlecheck: 'Average', titledisplay: '35', unitcheck: 'Km / hr' }
