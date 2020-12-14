613a2ae22e8c943eb5fd3f93a44f3b54c4c83574
PHENIXCRIME <photboatxeemus@icloud.com>
2020-12-14 17:02:23 +0700

    ADD link page

--- src/components/DataDisplay.js
+++ src/components/DataDisplay.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import styled from 'styled-components'
 import Display from '../components/DisplayDataDashboard/Display'
 import { Actioncontext } from '../context/Actioncontext'
+import { Link } from 'react-router-dom'
 
 const Showcase = styled.div`
   width: 80vw;
@@ -16,26 +17,34 @@ function DataDisplay () {
   const { temp, wind, humi, light } = useContext(Actioncontext)
   return (
     <Showcase>
+        <Link to='/temperature'>
       <Display
         titledata={temp.titledata}
         titlenumber={temp.titlenumber}
         titleunit={temp.titleunit}
       />
+        </Link>
+        <Link to='/wind'>
       <Display
         titledata={wind.titledata}
         titlenumber={wind.titlenumber}
         titleunit={wind.titleunit}
       />
+        </Link>
+        <Link to='/humidity'>
       <Display
         titledata={humi.titledata}
         titlenumber={humi.titlenumber}
         titleunit={humi.titleunit}
       />
+        </Link>
+        <Link to='/light'>
       <Display
         titledata={light.titledata}
         titlenumber={light.titlenumber}
         titleunit={light.titleunit}
       />
+        </Link>
     </Showcase>
   )
 }
