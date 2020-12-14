ae6f3a109217fcf76818a3dbcdf4d0638deefe3c
PHENIXCRIME <photboatxeemus@icloud.com>
2020-12-14 14:20:34 +0700

    SET value titlename dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -7,22 +7,31 @@ function Dashboard () {
   return (
     <Actioncontext.Provider
       value={{
+        dashboard: { titlename: ' Sensor Dashboard ' },
         temp: {
+          titlename: 'Temperature Dashboard',
           titledata: 'Temperature',
           titlenumber: '10',
           titleunit: 'celsius / °C'
         },
         wind: {
+          titlename: 'Wind Dashboard',
           titledata: 'Wind Speed',
           titlenumber: '20',
           titleunit: 'Km , Hr'
         },
         humi: {
+          titlename: 'Humidity Dashboard',
           titledata: 'Humidity',
           titlenumber: '30',
           titleunit: 'Percent / %'
         },
-        light: { titledata: 'Light', titlenumber: '40', titleunit: 'Lux' }
+        light: {
+          titelname: 'Light Dashboard',
+          titledata: 'Light',
+          titlenumber: '40',
+          titleunit: 'Lux'
+        }
       }}>
       <LayoutDashboard />
     </Actioncontext.Provider>
