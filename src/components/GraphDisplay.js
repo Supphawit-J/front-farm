bc96eed97564020abcbb4871255831849c63bb6e
PHENIXCRIME <photboatxeemus@icloud.com>
2020-12-15 06:16:09 +0700

    ADD styled glassmorphism

--- src/components/GraphDisplay.js
+++ src/components/GraphDisplay.js
@@ -16,6 +16,11 @@ justify-content : space-around ;
 const DisplayTop = styled.div`
 width : 78vw;
 height : 55vh ;
+/* background: rgba( 255, 255, 255, 0.25 );
+box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+backdrop-filter: blur( 4px );
+-webkit-backdrop-filter: blur( 4px );
+border-radius: 10px; */
 background-color : #fff ;
 border-radius : 1.5rem;
 box-shadow: 10px 11px 5px -8px rgba(0,0,0,0.32);
