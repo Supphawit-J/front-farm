4c4befa22d5c85684975a6b57346576afdb91c4d
PHENIXCRIME <photboatxeemus@icloud.com>
2020-12-15 06:15:16 +0700

    EDIT background

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -12,7 +12,9 @@ ${reset}
   font-size: 20px ; 
   scroll-behavior : smooth ;
   /* background-color : #282828 ; */
-  background: linear-gradient(90deg, rgba(0,90,167,1) 0%, rgba(255,253,228,1) 100%);
+  /* background: linear-gradient(90deg, rgba(0,90,167,1) 0%, rgba(255,253,228,1) 100%); */
+  /* background: linear-gradient(90deg, rgba(0,201,255,1) 0%, rgba(146,254,157,1) 100%); */
+  background: linear-gradient(90deg, rgba(0,0,70,1) 38%, rgba(28,181,224,1) 100%);
 }
 
 a { 
