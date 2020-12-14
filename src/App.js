b92745070cfc8e1a4ae24a5e29f2c55d518697fd
Supphawit-J <ideajeng2543@gmail.com>
2020-12-14 20:35:35 +0700

    ADD datacontext in app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Reset } from 'styled-reset'
 import withHelmet from './utils/withHelmet'
 import GlobalStyle from './components/GlobalStyle'
 import routes from './config/routes'
+import { DataContext } from './context/DataContext'
+import { GenerateData } from './utils/generateData'
 
 function App () {
+  const [{ bigData }, { useData }] = GenerateData()
+  const [temp, setTemp] = useState([])
+  const [deviceData, setDeviceData] = useState([])
+  const handleData = () => {
+    deviceData.push(
+      bigData.map(index => index)
+        .filter(device => device.device_id === 'D1'))
+  }
+  const handleTemp = () => {
+    temp.push(
+      deviceData.map(
+        index => ({ x: new Date(index.timestamp).getTime(), y: index.temp })
+      )
+    )
+  }
+
+  useEffect(() => {
+    useData()
+    handleData()
+    handleTemp()
+  }, [])
+
   return (
-    <>
+    <DataContext.Provider>
       <GlobalStyle/>
       <Reset/>
       <Router>
@@ -18,7 +42,7 @@ function App () {
           </Switch>
         </Suspense>
       </Router>
-    </>
+    </DataContext.Provider>
   )
 }
 
