50cc0fe9199915aabdb71496c75b7686d8ccc4cd
Supphawit-J <ideajeng2543@gmail.com>
2020-12-15 13:34:12 +0700

    EDIT light page

--- src/pages/Light.js
+++ src/pages/Light.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { DataContext } from '../context/DataContext'
 import LayoutData from '../components/LayoutData'
 
 import { Actioncontext } from '../context/Actioncontext'
 
 function Light () {
+  const { weekLight } = useContext(DataContext)
+
   return (
     <Actioncontext.Provider
       value={{
         titlename: 'Light Dasboard',
-        min: { titlecheck: 'Min', titledisplay: '10', unitcheck: 'Lux' },
-        max: { titlecheck: 'Max', titledisplay: '20', unitcheck: 'Lux' },
-        avg: { titlecheck: 'Average', titledisplay: '35', unitcheck: 'Lux' }
+        min: { titlecheck: 'Min', titledisplay: weekLight[6][0].min, unitcheck: 'Lux' },
+        max: { titlecheck: 'Max', titledisplay: weekLight[6][0].max, unitcheck: 'Lux' },
+        avg: { titlecheck: 'Average', titledisplay: weekLight[6][0].avg, unitcheck: 'Lux' }
       }}>
       <LayoutData />
     </Actioncontext.Provider>
