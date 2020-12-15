ff921d9efcbf18bd678f12ee314df5979630dc27
Supphawit-J <ideajeng2543@gmail.com>
2020-12-15 13:33:58 +0700

    EDIT humidity page

--- src/pages/Humidity.js
+++ src/pages/Humidity.js
@@ -1,26 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { DataContext } from '../context/DataContext'
 import LayoutData from '../components/LayoutData'
-
 import { Actioncontext } from '../context/Actioncontext'
 
 function Humidity () {
+  const { weekHumidity } = useContext(DataContext)
   return (
     <Actioncontext.Provider
       value={{
         titlename: 'Humidity Dashboard',
         min: {
           titlecheck: 'Min',
-          titledisplay: '10',
+          titledisplay: weekHumidity[6][0].min,
           unitcheck: 'Percent / %'
         },
         max: {
           titlecheck: 'Max',
-          titledisplay: '20',
+          titledisplay: weekHumidity[6][0].max,
           unitcheck: 'Percent / %'
         },
         avg: {
           titlecheck: 'Average',
-          titledisplay: '35',
+          titledisplay: weekHumidity[6][0].avg,
           unitcheck: 'Percent / %'
         }
       }}>
